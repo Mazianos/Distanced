81f94e985353887fc6f745a4466b272029ccc664
Rimpei2000 <rshiratori@my.bcit.ca>
2020-11-28 13:07:18 -0800

    Had to commit before pull

--- result.js
+++ result.js
@@ -115,3 +115,16 @@ function fastest() {
     routePath.setMap(map);
     routePathTwo.setMap(map);
 }
+document.getElementById('btn1').onclick = function() {	
+    var val1 = $('[name=origin]').val();	   
+    var val2 = $('[name=destination]').val();	  
+    var val3 = $('[name=time]').val();	
+    function save() {	
+        localStorage.setItem('current', val1);	
+        localStorage.setItem('destination', val2);	
+        localStorage.setItem('time', val3);	
+    }	
+    save()	
+    console.log(localStorage);	
+
+}
